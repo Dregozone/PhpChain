98d8bec2962450406ed45b0830381a4e8eb4ccd3
dz <dz@debian.home>
2021-02-21 09:41:59 +0000

    Some fabcoin example code

---
+++ 6-fabcoin/src/Block.php
@@ -0,0 +1,37 @@
+<?php 
+
+    Class Block 
+    {
+        public $previous;
+        public $nonce;
+        public $hash;
+        public $transaction;
+        
+        public function __construct(Transaction $transaction, ?Block $previous) {
+            
+            $this->previous = $previous ? $previous->hash : null;
+            $this->transaction = $transaction;
+            $this->mine();
+        }
+        
+        public static function createGenesis(string $pubKey, string $privKey, int $amount) {
+            return new self::(new Transaction(null, $pubKey, $amount, $privKey), null);
+        }
+        
+        public function mine() {
+            
+            $data = $this->message.$this->previous;
+            $this->nonce = PoW::findNonce($data);
+            $this->hash = PoW::hash($data.$this->nonce);
+        }
+        
+        public function isValid() : bool {
+            
+            return PoW::isValidNonce($this->message.$this->previous, $this->nonce);
+        }
+        
+        public function __toString() : string {
+            
+            return sprintf("Previous: %s\nNonce: %s\nHash: %s\nMessage: %s", $this->previous, $this->nonce, $this->hash, $this->message);
+        }
+    }
